1fd8cab5ffaf1260fa56bfe1cefc4eeb5fca8b92
jgowler <64489787+jgowler@users.noreply.github.com>
2025-10-25 19:34:32 +0100

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -6,6 +6,8 @@ sudo pacman -S --needed base-devel git rofi --noconfirm
 # Create Git folder
 mkdir -p "$HOME/git"
 
+$GIT = "$HOME/git"
+
 # Install yay (AUR helper)
 cd "$HOME/git"
 if [ ! -d "yay" ]; then
@@ -22,6 +24,15 @@ fi
 cd visual-studio-code-bin
 makepkg -si --noconfirm
  
-# Install Python:#
+# Install Python:
 yay -S python
 
+# Install Hyprland theme:
+cd $GIT
+git clone https://github.com/coffebar/dotfiles.git
+cd dotfiles
+mv ~/.config/hypr ~/.config/hypr.bak
+mv ~/.config/waybar ~/.config/waybar.bak
+mv ~/.config/rofi ~/.config/rofi.bak
+mv ~/.config/nvim ~/.config/nvim.bak
+sudo pacman -S hyprland waybar rofi --noconfirm
